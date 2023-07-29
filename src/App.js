0cdf449f0fb5d18d5e1c4355388295003f69cd77
ratnapriya2002 <chamalapriya@gmail.com>
2023-07-29 18:13:27 +0530

    Assignmentg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from "react";
+import { HashRouter,Switch, Route} from "react-router-dom";
+// import MenuBar from "./components/menubar/Menubar";
+import SignUp from "./components/program/SigninPage";
+import Login from "./components/program/LoginPage";
+// import DataPage from "./components/program/DataPage";
+// import MenuBar from "./components/menubar/Menubar";
+import MovieList from "./components/program/Datadisplay";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      {/* <MenuBar/> */}
+      <Switch>
+    
+        <Route exact path="/" component={SignUp} />
+        {/* <Link to="/login">Go to About Page</Link> */}
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/moviedata" component={MovieList} />
+        {/* <Route exact path="/menubar" component={MenuBar} /> */}
+      </Switch>
+  </HashRouter>
+  
+    
   );
-}
+};
 
 export default App;
